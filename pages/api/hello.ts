d83825701ce53f76f7c672566606354a6276f5e1
shyvesCCD <pedroochavess97@gmail.com>
2022-10-16 12:15:09 -0300

    chore: adding mailjet library, just testing.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function () {
+  const mailjet = require("node-mailjet").connect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+  );
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: "pilot@mailjet.com",
+          Name: "Mailjet Pilot",
+        },
+        To: [
+          {
+            Email: "passenger1@mailjet.com",
+            Name: "passenger 1",
+          },
+        ],
+        Subject: "Your email flight plan!",
+        TextPart:
+          "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+        HTMLPart:
+          '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+      },
+    ],
+  });
+  request
+    .then((result: any) => {
+      console.log(result.body);
+    })
+    .catch((err: any) => {
+      console.log(err.statusCode);
+    });
 }
