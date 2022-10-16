080a6de4b28caabd75e34f81db8615c9039b8871
shyvesCCD <pedroochavess97@gmail.com>
2022-10-15 21:28:04 -0300

    fix(_appBug): removing a ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
