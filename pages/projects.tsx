8d7aa82977278cbab84e9742c7378579c63c6826
shyvesCCD <pedroochavess97@gmail.com>
2022-12-29 17:22:38 -0300

    feat start to call the api

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,25 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { api } from "../lib/axios";
 
 const Projects: NextPage = () => {
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState([""]);
+  const [pagination, setPagination] = useState();
+
+  useEffect(() => {
+    try {
+      api.get("/categories").then((response) => {
+        setCategories(response.data.data);
+        setPagination(response.data.meta);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  console.log(categories);
+  console.log(pagination);
 
   return (
     <>
@@ -16,11 +32,3 @@ const Projects: NextPage = () => {
 };
 
 export default Projects;
-
-export const getServerSideProps = () => {
-  return {
-    props: {
-      list: [1, 2, 3],
-    },
-  };
-};
