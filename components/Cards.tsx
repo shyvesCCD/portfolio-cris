64b07c4c1b49fc3778953fbadbf7b637f8f09e41
shyvesCCD <pedroochavess97@gmail.com>
2023-01-02 21:30:25 -0300

    feat(Card) Component Card

---
+++ components/Cards.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+interface CardProps {
+  text: string;
+}
+
+const Card = ({ text }: CardProps) => {
+  const router = useRouter();
+
+  return (
+    <button
+      className="w-full max-h-56 mx-3 sm:w-1/4 md:w-1/3 text-center border-2 border-gray-400 rounded-sm hover:brightness-75"
+      onClick={() => router.push(`/projects/${text.toLowerCase()}`)}
+    >
+      <p className="font-semibold lg:text-2xl md:text-xl sm:text-sm">{text}</p>
+    </button>
+  );
+};
+
+export default Card;
