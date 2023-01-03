6f415cde67eccd7307f75c583b4da9c827aca403
shyvesCCD <pedroochavess97@gmail.com>
2023-01-02 21:31:28 -0300

    feat(Component) using component card

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { api } from "../lib/axios";
+import Card from "../components/Cards";
 
 interface ProjectsProps {
   data1: {
@@ -29,11 +30,11 @@ const Projects: NextPage<ProjectsProps> = ({ data1, data2 }) => {
       <div className="h-screen">
         <Header />
         {/*TODO: Agora preciso criar o componente que vai ficar as categorias */}
-        {data1.map((element) => (
-          <h1 className="font-bold" key={element.id}>
-            {element.attributes.category}
-          </h1>
-        ))}
+        <div className="h-5/6 flex justify-center flex-wrap">
+          {data1.map((element) => (
+            <Card text={element.attributes.category} key={element.id} />
+          ))}
+        </div>
       </div>
     </>
   );
@@ -55,6 +56,5 @@ export const getServerSideProps: GetStaticProps = async () => {
       data1: data.data.data,
       data2: data.data.meta,
     },
-    revalidate: 10,
   };
 };
