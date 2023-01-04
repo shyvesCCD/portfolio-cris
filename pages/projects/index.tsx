c03971c57d4c058907cfd719d5c9c2681b2e27e0
shyvesCCD <pedroochavess97@gmail.com>
2023-01-04 11:29:52 -0300

    feat(routes) create a dynamic route for categories

---
+++ pages/projects/index.tsx
@@ -0,0 +1,68 @@
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Header from "../../components/Header";
+import { api } from "../../lib/axios";
+import Card from "../../components/Cards";
+import { Categoria, Category } from "../../models/Category";
+
+const Projects: NextPage<Category> = ({ category }) => {
+  return (
+    <>
+      <div className="h-screen">
+        <Header />
+        {/*TODO: Agora preciso criar o componente que vai ficar as categorias */}
+        <div className="h-5/6 flex justify-center flex-wrap">
+          {category ? (
+            category.map((element) => (
+              <Card text={element.attributes.category} key={element.id} />
+            ))
+          ) : (
+            <h1>Não foi possível carregar as categorias.</h1>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
+
+//export const getStaticPaths: GetStaticPaths = async () => {
+//const response = await api.get("/categories", {
+//headers: {
+//Accept: "application/json",
+//},
+//});
+
+//const data1 = response.data.data;
+
+//const paths = data1.map((categories: Categoria) => ({
+//params: { category: categories.attributes.category },
+//}));
+
+//return {
+//paths,
+//fallback: false,
+//};
+//};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const response = await api.get("categories", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const data1 = response.data.data;
+
+  if (!data1) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      category: data1,
+    },
+  };
+};
