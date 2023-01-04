ecd064fd578aa86e2c3f5c9923b7fa3bda9ca29d
shyvesCCD <pedroochavess97@gmail.com>
2023-01-04 10:17:03 -0300

    fix(space) removing the white space

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -10,7 +10,9 @@ const Card = ({ text }: CardProps) => {
   return (
     <button
       className="w-full max-h-56 mx-3 sm:w-1/4 md:w-1/3 text-center border-2 border-gray-400 rounded-sm hover:brightness-75"
-      onClick={() => router.push(`/projects/${text.toLowerCase()}`)}
+      onClick={() =>
+        router.push(`/projects/${text.toLowerCase().replace(/ /g, "")}`)
+      }
     >
       <p className="font-semibold lg:text-2xl md:text-xl sm:text-sm">{text}</p>
     </button>
