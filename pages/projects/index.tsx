be5095018912fb204beaabb0661597e7602a1471
shyvesCCD <pedroochavess97@gmail.com>
2023-01-07 12:31:59 -0300

    fix(type) fix the type and removing the todo.

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,17 +2,16 @@ import type { NextPage } from "next";
 import Header from "../../components/Header";
 import { api } from "../../lib/axios";
 import Card from "../../components/Cards";
-import { Category } from "../../models/Category";
+import { Category, ElementProps } from "../../models/Category";
 
 const Projects: NextPage<Category> = ({ category }) => {
   return (
     <>
       <div className="h-screen">
         <Header />
-        {/*TODO: Agora preciso criar o componente que vai ficar as categorias */}
         <div className="h-5/6 flex justify-center flex-wrap">
           {category ? (
-            category.map((element) => (
+            category.map((element: ElementProps) => (
               <Card text={element.attributes.category} key={element.id} />
             ))
           ) : (
