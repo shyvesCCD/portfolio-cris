04eaf15dcb2e028d5dd1d64f12d90cd69a52925f
shyvesCCD <pedroochavess97@gmail.com>
2023-01-10 13:27:05 -0300

    refactor: create a function to fetch data

---
+++ lib/load-category.ts
@@ -0,0 +1,18 @@
+import { ElementProps, ParamsProps } from "../models/Category";
+import { api } from "./axios";
+
+export async function loadCategory(params: ParamsProps) {
+  const { data } = await api.get("/categories");
+
+  if (!data) return { notFound: true };
+
+  const response = data.data;
+
+  const value = response.filter(
+    (element: ElementProps) =>
+      element.attributes.category.toLowerCase().replace(/\s/g, "") ==
+      params.category
+  );
+
+  return value;
+}
