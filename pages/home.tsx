f1c781e683889769eaaa2a7f8c363df95221c0eb
shyvesCCD <pedroochavess97@gmail.com>
2023-01-10 15:44:50 -0300

    feat(responsive) make the home page responsive.

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,18 +9,28 @@ const InicialPage: NextPage = () => {
     <div className="h-screen">
       <Header />
       {/*TODO: Ajeitar as fotos para que elas ocupem o site inteiro.*/}
-      <main className="grid grid-cols-3 h-5/6">
-        <Image src={profilePic} alt="Profile picture" objectFit="cover" />
+      <main className="lg:grid lg:grid-cols-3 flex flex-col justify-center h-5/6">
+        <Image
+          className="lg:visible invisible"
+          src={profilePic}
+          alt="Profile picture"
+          objectFit="cover"
+        />
         <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-4xl mb-8">Hi, I’m Cris.</h1>
-          <p className="text-2xl mb-4">
+          <h1 className="lg:text-4xl text-5xl font-bold mb-8">Hi, I’m Cris.</h1>
+          <p className="lg:text-2xl lg:mx-0 text-xl mb-4 mx-5">
             I’m a video editor based in Porto, Portugal.
           </p>
-          <p className="text-2xl">
+          <p className="lg:text-2xl lg:mx-0 text-xl mx-5">
             But I can help you tell your story wherever you are.
           </p>
         </div>
-        <Image src={setupCris} alt="Setup of the author" objectFit="cover" />
+        <Image
+          className="lg:visible invisible"
+          src={setupCris}
+          alt="Setup of the author"
+          objectFit="cover"
+        />
       </main>
     </div>
   );
