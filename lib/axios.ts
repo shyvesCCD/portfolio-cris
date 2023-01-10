c62de56e12cacf44f66e2e077499173be498b940
shyvesCCD <pedroochavess97@gmail.com>
2023-01-09 23:07:22 -0300

    fix(url) forget the /api

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://cris-backend-production.up.railway.app/",
+    baseURL: "https://cris-backend-production.up.railway.app/api",
 });
