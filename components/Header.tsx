8dfdd80e8339924b00a2b09374403e8e4b589c11
shyvesCCD <pedroochavess97@gmail.com>
2023-01-11 15:18:18 -0300

    feat(nav) create responsive header with hmenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,19 +5,19 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <header className="flex justify-between p-8 h-1/6 items-center">
+    <nav className="sticky top-0 left-0 w-full h-1/6 flex justify-between items-center">
       <Link href="/home" passHref>
-        <a className="text-2xl font-medium">
+        <a className="text-2xl pl-8 py-4 font-medium">
           Cris Aldreyn | video editor services
         </a>
       </Link>
-      <ul>
-        <Link href="/home" passHref>
+      <ul className="hidden md:flex items-center text-base cursor-pointer">
+        <Link href="home" passHref>
           <a
             className={
               router.pathname == "/home"
-                ? "font-bold hover:brightness-90 text-xl"
-                : "hover:brightness-90 text-xl"
+                ? "font-bold px-6 py-4 hover:brightness-90 text-xl"
+                : "px-6 py-4 hover:brightness-90 text-xl"
             }
           >
             home
@@ -27,8 +27,8 @@ const Header = () => {
           <a
             className={
               router.pathname == "/about"
-                ? "font-bold ml-8 hover:brightness-90 text-xl"
-                : "ml-8 hover:brightness-90 text-xl"
+                ? "font-bold px-6 py-4 hover:brightness-90 text-xl"
+                : "px-6 py-4 hover:brightness-90 text-xl"
             }
           >
             about
@@ -38,26 +38,79 @@ const Header = () => {
           <a
             className={
               router.pathname == "/projects"
-                ? "font-bold ml-8 hover:brightness-90 text-xl"
-                : "ml-8 hover:brightness-90 text-xl"
+                ? "font-bold px-6 py-4 hover:brightness-90 text-xl"
+                : "px-6 py-4 hover:brightness-90 text-xl"
             }
           >
             projects
           </a>
         </Link>
-        <Link href="services" passHref>
+        <Link href="/services" passHref>
           <a
             className={
               router.pathname == "/services"
-                ? "font-bold ml-8 hover:brightness-90 text-xl"
-                : "ml-8 hover:brightness-90 text-xl"
+                ? "font-bold px-6 py-4 hover:brightness-90 text-xl"
+                : "px-6 py-4 hover:brightness-90 text-xl"
             }
           >
             services
           </a>
         </Link>
       </ul>
-    </header>
+      <button className="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:brightness-90 group">
+        <div className="w-5 h-1 bg-gray-600 mb-1 rounded"></div>
+        <div className="w-5 h-1 bg-gray-600 mb-1 rounded"></div>
+        <div className="w-5 h-1 bg-gray-600 mb-1 rounded"></div>
+        <div className="absolute top-30 -right-full w-2/12 opacity-0 group-focus:right-10 group-focus:opacity-100 transition-all duration-300 bg-zinc-800 rounded-md">
+          <ul className="flex flex-col items-center w-full pt-4">
+            <Link href="home" passHref>
+              <a
+                className={
+                  router.pathname == "/home"
+                    ? "w-full font-bold px-6 pb-4 hover:brightness-90 text-xl"
+                    : "w-full px-6 py-4 hover:brightness-90 text-xl"
+                }
+              >
+                home
+              </a>
+            </Link>
+            <Link href="about" passHref>
+              <a
+                className={
+                  router.pathname == "/about"
+                    ? "w-full font-bold px-6 py-4 hover:brightness-90 text-xl"
+                    : "w-full px-6 py-4 hover:brightness-90 text-xl"
+                }
+              >
+                about
+              </a>
+            </Link>
+            <Link href="projects" passHref>
+              <a
+                className={
+                  router.pathname == "/projects"
+                    ? "w-full font-bold px-6 py-4 hover:brightness-90 text-xl"
+                    : "w-full px-6 py-4 hover:brightness-90 text-xl"
+                }
+              >
+                projects
+              </a>
+            </Link>
+            <Link href="services" passHref>
+              <a
+                className={
+                  router.pathname == "/services"
+                    ? "w-full font-bold px-6 py-4 hover:brightness-90 text-xl"
+                    : "w-full px-6 py-4 hover:brightness-90 text-xl"
+                }
+              >
+                services
+              </a>
+            </Link>
+          </ul>
+        </div>
+      </button>
+    </nav>
   );
 };
 
