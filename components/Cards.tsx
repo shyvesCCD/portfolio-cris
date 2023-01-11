d1281ef638196b6219882f43e2d1ba4b69829338
shyvesCCD <pedroochavess97@gmail.com>
2023-01-11 15:37:41 -0300

    feat(style) make more responsive cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -9,12 +9,12 @@ const Card = ({ text }: CardProps) => {
 
   return (
     <button
-      className="w-full max-h-56 mx-3 sm:w-1/4 md:w-1/3 text-center border-2 border-gray-400 rounded-sm hover:brightness-75"
+      className="lg:w-3/12 lg:h-48 lg:mt-0 mt-7 w-2/3 h-36 mx-9 text-center border-2 border-gray-400 rounded-sm hover:brightness-75"
       onClick={() =>
         router.push(`/projects/${text.toLowerCase().replace(/ /g, "")}`)
       }
     >
-      <p className="font-semibold lg:text-2xl md:text-xl sm:text-sm">{text}</p>
+      <p className="lg:text-2xl text-xl">{text.toUpperCase()}</p>
     </button>
   );
 };
