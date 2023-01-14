50f0ddbb6be26ad51381cf9a42a08b4ebf03474b
shyvesCCD <pedroochavess97@gmail.com>
2023-01-13 22:04:44 -0300

    feat(categoryPage) responsive design

--- pages/projects/[category].tsx
+++ pages/projects/[category].tsx
@@ -1,13 +1,15 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import Header from "../../components/Header";
+import HeaderCategories from "../../components/HeaderCategories";
 import { api } from "../../lib/axios";
 import { loadCategory } from "../../lib/load-category";
 import { Categoria, CategoryWhenNotArray } from "../../models/Category";
 
 const ProjectsCategory: NextPage<CategoryWhenNotArray> = ({
   category,
-  text,
+  textArray,
+  responseArray,
 }) => {
   const router = useRouter();
 
@@ -16,23 +18,36 @@ const ProjectsCategory: NextPage<CategoryWhenNotArray> = ({
   }
 
   return (
-    <div className="h-screen">
+    <div className="h-screen flex flex-col items-center justify-center">
       <Header />
-      <main className="mt-[15vh] h-[85vh] flex flex-col items-center justify-center lg:flex-row mx-20">
-        <div className="w-full lg:w-1/3 lg:mt-0 mt-4 flex items-center justify-center">
-          <div className="relative h-0 w-full pb-aspect">
-            <iframe
-              src={category.attributes.linkURL}
-              className="absolute top-0 left-0 w-full h-full mx-auto "
-              allowFullScreen
-            ></iframe>
-          </div>
+      <main className="mt-[15vh] h-[85vh] lg:flex-row mx-20">
+        <div className="container flex items-center justify-center text-center flex-wrap">
+          <HeaderCategories categories={responseArray} />
         </div>
-        <div className="flex text-base w-full lg:w-2/3 overflow-y-scroll lg:m-14 my-10 max-h-[38rem]">
-          <div
-            className="lg:ml-16 ml-0"
-            dangerouslySetInnerHTML={{ __html: text }}
-          />
+        <div className="flex lg:flex-row flex-col">
+          <div className="w-full lg:w-1/3 lg:mt-0 mt-4 flex items-center justify-center">
+            <div className="relative h-0 w-full pb-aspect">
+              <iframe
+                src={category.attributes.linkURL}
+                className="absolute top-0 left-0 w-full h-full mx-auto "
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+          <div className="flex flex-col text-base w-full lg:w-2/3 overflow-y-scroll lg:m-14 my-10 max-h-[38rem]">
+            <h1 className="text-center mb-9 font-medium text-4xl">
+              {category.attributes.category}
+            </h1>
+            <div className="lg:ml-16 ml-0 ">
+              {textArray.map((paragraph) => (
+                <p
+                  key={textArray.indexOf(paragraph)}
+                  className="indent-4 mb-4 last:indent-0 last:mb-0 text-justify"
+                  dangerouslySetInnerHTML={{ __html: paragraph }}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </main>
     </div>
@@ -55,14 +70,21 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async (context) => {
   const params = context.params!;
 
-  const value = await loadCategory(params);
+  const { value, response } = await loadCategory(params);
 
-  const text = value[0].attributes.description.replace(/\n/g, "<br />");
+  const textArray = value[0].attributes.description.split("\n");
+
+  const responseArray: string[] = [];
+
+  response.map((element: any) => {
+    responseArray.push(element.attributes.category.toLowerCase());
+  });
 
   return {
     props: {
       category: value[0],
-      text,
+      textArray,
+      responseArray,
     },
   };
 };
