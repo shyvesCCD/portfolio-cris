c1437c48f1c567724377500712ec5353954e808e
shyvesCCD <pedroochavess97@gmail.com>
2023-01-18 14:30:19 -0300

    fix(responsive) adjust sidebar responsive mobile

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,46 +9,44 @@ const SideBar = ({ handleClickClear }: SideBarProps) => {
   const router = useRouter();
 
   return (
-    <nav className="flex justify-center">
-      <div className="h-full flex flex-col place-content-center">
-        <Link href="/about" passHref>
-          <a
-            onClick={(e) => handleClickClear(e, "/about")}
-            className={
-              router.asPath == "/about"
-                ? "mb-9 font-bold hover:brightness-90 text-xl"
-                : "mb-9 hover:brightness-90 text-xl"
-            }
-          >
-            skills & tools
-          </a>
-        </Link>
-        <Link href="/experience" passHref>
-          <a
-            onClick={(e) => handleClickClear(e, "/experience")}
-            className={
-              router.asPath == "/experience"
-                ? "mb-9 font-bold hover:brightness-90 text-xl"
-                : "mb-9 hover:brightness-90 text-xl"
-            }
-          >
-            experience
-          </a>
-        </Link>
-        <Link href="/education" passHref>
-          <a
-            onClick={(e) => handleClickClear(e, "/education")}
-            className={
-              router.asPath == "/education"
-                ? "font-bold hover:brightness-90 text-xl"
-                : "hover:brightness-90 text-xl"
-            }
-          >
-            education
-          </a>
-        </Link>
-      </div>
-    </nav>
+    <div className="h-full flex lg:flex-col flex-row items-center lg:justify-center">
+      <Link href="/about" passHref>
+        <a
+          onClick={(e) => handleClickClear(e, "/about")}
+          className={
+            router.asPath == "/about"
+              ? "lg:mb-9 lg:mx-0 mx-6 font-bold hover:brightness-90 text-xl"
+              : "lg:mb-9 lg:mx-0 mx-6 hover:brightness-90 text-xl"
+          }
+        >
+          skills & tools
+        </a>
+      </Link>
+      <Link href="/experience" passHref>
+        <a
+          onClick={(e) => handleClickClear(e, "/experience")}
+          className={
+            router.asPath == "/experience"
+              ? "lg:mb-9 lg:mx-0 mx-6 font-bold hover:brightness-90 text-xl"
+              : "lg:mb-9 lg:mx-0 mx-6 hover:brightness-90 text-xl"
+          }
+        >
+          experience
+        </a>
+      </Link>
+      <Link href="/education" passHref>
+        <a
+          onClick={(e) => handleClickClear(e, "/education")}
+          className={
+            router.asPath == "/education"
+              ? "lg:mb-9 lg:mx-0 mx-6 font-bold hover:brightness-90 text-xl"
+              : "lg:mb-9 lg:mx-0 mx-6 hover:brightness-90 text-xl"
+          }
+        >
+          education
+        </a>
+      </Link>
+    </div>
   );
 };
 
