ad940c268086cb3dfd97c05bc9df9e14cb09423b
shyvesCCD <pedroochavess97@gmail.com>
2023-01-20 16:49:46 -0300

    fix(position) fix the position on watermark

--- components/Watermark.tsx
+++ components/Watermark.tsx
@@ -2,7 +2,7 @@ import { FiMail } from "react-icons/fi";
 
 const WaterMark = () => {
   return (
-    <div className="bg-zinc-900 hover:w-16 hover:h-16 transition-all w-14 h-14 rounded-md absolute bottom-2 right-2 flex items-center justify-center cursor-pointer">
+    <div className="bg-zinc-900 hover:w-16 hover:h-16 transition-all w-14 h-14 rounded-md absolute bottom-3 right-3 flex items-center justify-center cursor-pointer">
       <FiMail className="fill-zinc-800 w-7 h-7" />
     </div>
   );
