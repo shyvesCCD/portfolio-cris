6ff001231c773ef573c99d1f8b464df7a9cfd5c5
shyvesCCD <pedroochavess97@gmail.com>
2023-01-21 13:34:07 -0300

    feat(services) watermark now push for service page

--- components/Watermark.tsx
+++ components/Watermark.tsx
@@ -1,8 +1,14 @@
+import { useRouter } from "next/router";
 import { FiMail } from "react-icons/fi";
 
 const WaterMark = () => {
+  const router = useRouter();
+
   return (
-    <div className="bg-zinc-900 hover:w-16 hover:h-16 transition-all w-14 h-14 rounded-md absolute bottom-3 right-3 flex items-center justify-center cursor-pointer">
+    <div
+      onClick={() => router.push("/services")}
+      className="bg-zinc-900 hover:w-16 hover:h-16 transition-all w-14 h-14 rounded-md absolute bottom-3 right-3 flex items-center justify-center cursor-pointer"
+    >
       <FiMail className="fill-zinc-800 w-7 h-7" />
     </div>
   );
