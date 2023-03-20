38291258b1826f2e6ce457459370ed13dd635306
shyvesCCD <pedroochavess97@gmail.com>
2023-03-20 13:37:10 -0300

    Change the way we present the categories

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,20 +1,49 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 interface CardProps {
+  image: any;
   text: string;
 }
 
-const Card = ({ text }: CardProps) => {
+const Card = ({ image, text }: CardProps) => {
+  const [isHovering, setIsHovered] = useState(false);
+  const onMouseEnter = () => setIsHovered(true);
+  const onMouseLeave = () => setIsHovered(false);
   const router = useRouter();
 
   return (
     <button
-      className="lg:w-3/12 lg:h-48 lg:mt-0 mt-7 w-2/3 h-36 mx-9 text-center border border-gray-400 rounded-md hover:brightness-75"
+      className="mx-20 transition-all hover:brightness-90 hover:font-bold flex justify-center items-center flex-col text-zinc-100 hover:text-black"
       onClick={() =>
         router.push(`/projects/${text.toLowerCase().replace(/ /g, "")}`)
       }
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
-      <p className="lg:text-2xl text-xl capitalize">{text}</p>
+      {isHovering ? (
+        <>
+          <Image
+            alt=""
+            src={`https://cris-backend-production.up.railway.app${image}`}
+            width={180}
+            height={350}
+          />
+          <p className="lg:text-2xl text-xl capitalize">{text}</p>
+        </>
+      ) : (
+        <>
+          <Image
+            className=""
+            alt=""
+            src={`https://cris-backend-production.up.railway.app${image}`}
+            width={180}
+            height={350}
+          />
+          <p className="lg:text-2xl text-xl capitalize">{text}</p>
+        </>
+      )}
     </button>
   );
 };
