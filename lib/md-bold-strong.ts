88898a3102d4f62c015698c8198098bbd5822f5d
shyvesCCD <pedroochavess97@gmail.com>
2023-03-20 13:37:56 -0300

    check if the text is null

--- lib/md-bold-strong.ts
+++ lib/md-bold-strong.ts
@@ -1,6 +1,11 @@
 export function mdBoldToStrong(text: string) {
-    const surrounding = new RegExp(/\*{2}([^*]+)\*{2}/g);
-    const newText = text.replace(surrounding, "<strong>$1</strong>");
-
-    return newText.replaceAll("\n", "<br>");
+    if (text == null) {
+        return null
+    } else {
+        const surrounding = new RegExp(/\*{2}([^*]+)\*{2}/g);
+        const newText = text.replace(surrounding, "<strong>$1</strong>");
+    
+        
+        return newText.replaceAll("\n", "<br>");
+    }
 }
