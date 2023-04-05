907543ad390265f59c29a03cded928bd50e99224
shyvesCCD <pedroochavess97@gmail.com>
2023-04-04 22:03:25 -0300

    style: improve the format

--- lib/md-bold-strong.ts
+++ lib/md-bold-strong.ts
@@ -1,11 +1,10 @@
 export function mdBoldToStrong(text: string) {
     if (text == null) {
-        return null
+        return null;
     } else {
         const surrounding = new RegExp(/\*{2}([^*]+)\*{2}/g);
         const newText = text.replace(surrounding, "<strong>$1</strong>");
-    
-        
+
         return newText.replaceAll("\n", "<br>");
     }
 }
