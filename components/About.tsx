a5cb6a87143b17af13707f4d93b1ea47380a70df
shyvesCCD <pedroochavess97@gmail.com>
2023-04-10 12:55:04 -0300

    feat: fix the about component

--- components/About.tsx
+++ components/About.tsx
@@ -2,79 +2,74 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 
 type AboutProps = {
-  image: any;
-  description: string;
-  description2: string | null;
-  image2: any | null;
+    image: any;
+    description: string;
+    description2: string | null;
+    image2: any | null;
 };
 
 const AboutPageComponent = ({
-  image,
-  description,
-  description2,
-  image2,
+    image,
+    description,
+    description2,
+    image2,
 }: AboutProps) => {
-  const { theme } = useTheme();
+    const { theme } = useTheme();
 
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-5">
-        {theme == "light" ? (
-          <Image
-            src={`https://cris-backend-production.up.railway.app${image}`}
-            alt=""
-            width={400}
-            height={300}
-          />
-        ) : (
-          <Image
-            className="invert"
-            src={`https://cris-backend-production.up.railway.app${image}`}
-            alt=""
-            width={400}
-            height={300}
-          />
-        )}
-        <p
-          className="flex flex-col text-2xl text-center"
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></p>
-      </div>
-      <div className="flex items-center justify-center flex-wrap">
+    return (
         <>
-          {description2 ? (
-            <p
-              className="flex flex-col text-center text-2xl"
-              dangerouslySetInnerHTML={{ __html: description2 }}
-            ></p>
-          ) : (
-            image2.data.map((element: any, index: any) => (
-              <div key={index} className="mx-10">
+            <div className="flex flex-col items-center justify-center gap-5">
                 {theme == "light" ? (
-                  <Image
-                    key={index}
-                    alt=""
-                    src={`https://cris-backend-production.up.railway.app${element.attributes.url}`}
-                    width={200}
-                    height={120}
-                  />
+                    <Image src={`${image}`} alt="" width={400} height={300} />
                 ) : (
-                  <Image
-                    className="invert"
-                    key={index}
-                    alt=""
-                    src={`https://cris-backend-production.up.railway.app${element.attributes.url}`}
-                    width={200}
-                    height={120}
-                  />
+                    <Image
+                        className="invert"
+                        src={`${image}`}
+                        alt=""
+                        width={400}
+                        height={300}
+                    />
                 )}
-              </div>
-            ))
-          )}
+                <p
+                    className="flex flex-col text-2xl text-center"
+                    dangerouslySetInnerHTML={{ __html: description }}
+                ></p>
+            </div>
+            <div className="flex items-center justify-center flex-wrap">
+                <>
+                    {description2 ? (
+                        <p
+                            className="flex flex-col text-center text-2xl"
+                            dangerouslySetInnerHTML={{ __html: description2 }}
+                        ></p>
+                    ) : (
+                        image2.data.map((element: any, index: any) => (
+                            <div key={index} className="mx-10">
+                                {theme == "light" ? (
+                                    <Image
+                                        key={index}
+                                        alt=""
+                                        src={`${element.attributes.url}`}
+                                        width={200}
+                                        height={120}
+                                    />
+                                ) : (
+                                    <Image
+                                        className="invert"
+                                        key={index}
+                                        alt=""
+                                        src={`${element.attributes.url}`}
+                                        width={200}
+                                        height={120}
+                                    />
+                                )}
+                            </div>
+                        ))
+                    )}
+                </>
+            </div>
         </>
-      </div>
-    </>
-  );
+    );
 };
 
 export default AboutPageComponent;
