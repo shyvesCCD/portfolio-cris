c5d215c0e8cd365c72d618ee62131a4325c78f70
shyvesCCD <pedroochavess97@gmail.com>
2023-04-10 12:56:52 -0300

    fix: the return and theme

--- pages/projects/[category].tsx
+++ pages/projects/[category].tsx
@@ -8,115 +8,116 @@ import WaterMark from "../../components/Watermark";
 import { api } from "../../lib/axios";
 import { loadCategory } from "../../lib/load-category";
 import { Categoria, CategoryWhenNotArray } from "../../models/Category";
-//import TWOHANDS from "../../public/TWOHANDS.svg";
 
 const ProjectsCategory: NextPage<CategoryWhenNotArray> = ({
-  retorno,
-  category,
-  textArray,
-  responseArray,
+    retorno,
+    textArray,
+    responseArray,
 }) => {
-  const router = useRouter();
-  const { theme } = useTheme();
+    const router = useRouter();
+    const { theme } = useTheme();
 
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div className="h-screen flex flex-col">
-      <Header />
-      <main className="h-[85vh] mt-[15vh] lg:flex-row mx-10">
-        <div className="flex items-center justify-center text-center flex-wrap">
-          <HeaderCategories categories={responseArray} />
+    return (
+        <div className="h-screen flex flex-col">
+            <Header />
+            <main className="h-[85vh] mt-[15vh] lg:flex-row mx-10">
+                <div className="flex items-center justify-center text-center flex-wrap">
+                    <HeaderCategories categories={responseArray} />
+                </div>
+                <div className="flex lg:flex-row flex-col">
+                    <div className="lg:visible invisible flex items-center">
+                        {theme == "light" ? (
+                            <Image
+                                className="lg:visible invisible"
+                                src={`${retorno.attributes.fotoCategoria.data[0].attributes.url}`}
+                                alt=""
+                                objectFit="contain"
+                                width={500}
+                                height={500}
+                            />
+                        ) : (
+                            <Image
+                                className="lg:visible invisible invert"
+                                src={`${retorno.attributes.fotoCategoria.data[0].attributes.url}`}
+                                alt=""
+                                objectFit="contain"
+                                width={500}
+                                height={500}
+                            />
+                        )}
+                    </div>
+                    <div className="flex flex-col text-base w-full lg:w-2/3 overflow-auto lg:mr-10 my-10 max-h-[38rem]">
+                        <h1 className="text-center mb-9 font-medium text-4xl">
+                            {retorno.attributes.category}
+                        </h1>
+                        <div className="lg:ml-16 ml-0 max-h-[512px] mr-10">
+                            {textArray.map((paragraph) => (
+                                <p
+                                    key={textArray.indexOf(paragraph)}
+                                    className="indent-4 mb-4 last:indent-0 last:mb-0 text-justify"
+                                    dangerouslySetInnerHTML={{
+                                        __html: paragraph,
+                                    }}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-1/3 lg:mt-0 mt-4 flex items-center justify-center">
+                        <div className="relative h-0 w-full pb-aspect">
+                            <iframe
+                                src={retorno.attributes.linkUrl}
+                                className="absolute top-0 left-0 w-full h-full mx-auto "
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <WaterMark />
         </div>
-        <div className="flex lg:flex-row flex-col">
-          <div className="lg:visible invisible flex items-center">
-            {theme == "light" ? (
-              <Image
-                className="lg:visible invisible"
-                src={`https://cris-backend-production.up.railway.app${retorno.attributes.fotoCategoria.data.attributes.url}`}
-                alt=""
-                objectFit="contain"
-                width={500}
-                height={500}
-              />
-            ) : (
-              <Image
-                className="lg:visible invisible invert"
-                src={`https://cris-backend-production.up.railway.app${retorno.attributes.fotoCategoria.data.attributes.url}`}
-                alt=""
-                objectFit="contain"
-                width={500}
-                height={500}
-              />
-            )}
-          </div>
-          <div className="flex flex-col text-base w-full lg:w-2/3 overflow-auto lg:mr-10 my-10 max-h-[38rem]">
-            <h1 className="text-center mb-9 font-medium text-4xl">
-              {category.attributes.category}
-            </h1>
-            <div className="lg:ml-16 ml-0 max-h-[512px] mr-10">
-              {textArray.map((paragraph) => (
-                <p
-                  key={textArray.indexOf(paragraph)}
-                  className="indent-4 mb-4 last:indent-0 last:mb-0 text-justify"
-                  dangerouslySetInnerHTML={{ __html: paragraph }}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="w-full lg:w-1/3 lg:mt-0 mt-4 flex items-center justify-center">
-            <div className="relative h-0 w-full pb-aspect">
-              <iframe
-                src={category.attributes.linkURL}
-                className="absolute top-0 left-0 w-full h-full mx-auto "
-                allowFullScreen
-              ></iframe>
-            </div>
-          </div>
-        </div>
-      </main>
-      <WaterMark />
-    </div>
-  );
+    );
 };
 
 export default ProjectsCategory;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await api.get("/categories");
-  const response = data.data;
+    const { data } = await api.get("/categories");
+    const response = data.data;
 
-  const paths = response.map(({ attributes }: Categoria) => ({
-    params: { category: attributes.category.replace(/\s/, "").toLowerCase() },
-  }));
+    const paths = response.map(({ attributes }: Categoria) => ({
+        params: {
+            category: attributes.category.replace(/\s/, "").toLowerCase(),
+        },
+    }));
 
-  return { paths, fallback: "blocking" };
+    return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const params = context.params!;
+    const params = context.params!;
 
-  const { value, response } = await loadCategory(params);
+    const { value, response } = await loadCategory(params);
 
-  const textArray = value[0].attributes.description.split("\n");
+    const textArray = value[0].attributes.description.split("\n");
 
-  const retorno = value[0];
+    const retorno = value[0];
 
-  const responseArray: string[] = [];
+    const responseArray: string[] = [];
 
-  response.map((element: any) => {
-    responseArray.push(element.attributes.category.toLowerCase());
-  });
+    response.map((element: any) => {
+        responseArray.push(element.attributes.category.toLowerCase());
+    });
 
-  return {
-    props: {
-      retorno,
-      category: value[0],
-      textArray,
-      responseArray,
-    },
-    revalidate: 60,
-  };
+    return {
+        props: {
+            retorno,
+            textArray,
+            responseArray,
+        },
+        revalidate: 60,
+    };
 };
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -6,47 +6,44 @@ import { Category, ElementProps } from "../../models/Category";
 import WaterMark from "../../components/Watermark";
 
 const Projects: NextPage<Category> = ({ category }) => {
-  return (
-    <>
-      <div className="h-screen flex flex-col">
-        <Header />
-        <div className="mt-[15vh] h-[85vh] flex items-center justify-center flex-wrap">
-          {category ? (
-            category.map((element: ElementProps) => (
-              <Card
-                image={element.attributes.fotoCategoria.data.attributes.url}
-                text={element.attributes.category}
-                key={element.id}
-              />
-            ))
-          ) : (
-            <h1>Não foi possível carregar as categorias.</h1>
-          )}
-        </div>
-        <WaterMark />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="h-screen flex flex-col">
+                <Header />
+                <div className="mt-[15vh] h-[85vh] flex items-center justify-center flex-wrap">
+                    {category ? (
+                        category.map((element: ElementProps) => (
+                            <Card
+                                image={
+                                    element.attributes.fotoCategoria.data[0]
+                                        .attributes.url
+                                }
+                                text={element.attributes.category}
+                                key={element.id}
+                            />
+                        ))
+                    ) : (
+                        <h1>Não foi possível carregar as categorias.</h1>
+                    )}
+                </div>
+                <WaterMark />
+            </div>
+        </>
+    );
 };
 
 export default Projects;
 
 export const getServerSideProps = async () => {
-  const response = await api.get("categories?populate=*", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+    const response = await api.get("/categories?populate=*");
 
-  const data1 = response.data.data;
+    const data1 = response.data.data;
 
-  console.log(data1);
+    if (!data1) return { notFound: true };
 
-  if (!data1) return { notFound: true };
-
-  return {
-    props: {
-      category: data1,
-    },
-  };
+    return {
+        props: {
+            category: data1,
+        },
+    };
 };
