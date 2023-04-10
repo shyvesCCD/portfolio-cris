3338eb23bbb86b6d8d1e70ea8b890c30430e34f2
shyvesCCD <pedroochavess97@gmail.com>
2023-04-10 12:56:30 -0300

    style: formating category models

--- models/Category.ts
+++ models/Category.ts
@@ -1,87 +1,87 @@
 /*TODO: Pensar em melhores nomes para as interfaces */
 
 export interface ParamsProps {
-  category?: string;
+    category?: string;
 }
 
 export interface ParamsPropsAbout {
-  title?: string;
+    title?: string;
 }
 
 export interface Category {
-  category: {
-    id: number;
-    attributes: {
-      category: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
-      description: string;
-      linkURL: string;
-      fotoCategoria: any;
-    };
-  }[];
-  text: string;
+    category: {
+        id: number;
+        attributes: {
+            category: string;
+            createdAt: string;
+            updatedAt: string;
+            publishedAt: string;
+            description: string;
+            linkURL: string;
+            fotoCategoria: any;
+        };
+    }[];
+    text: string;
 }
 
 export interface AboutProps {
-  data: any;
-  textArray: string[];
-  textArray2: string[];
+    data: any;
+    textArray: string[];
+    textArray2: string[];
 }
 
 /*TODO!: Preciso melhorar o nome dessas interfaces urgente.*/
 export interface CategoryWhenNotArray {
-  retorno: any;
-  category: {
-    id: number;
-    attributes: {
-      category: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
-      description: string;
-      linkURL: string;
+    retorno: any;
+    category: {
+        id: number;
+        attributes: {
+            category: string;
+            createdAt: string;
+            updatedAt: string;
+            publishedAt: string;
+            description: string;
+            linkURL: string;
+        };
     };
-  };
 
-  textArray: string[];
-  responseArray: string[];
+    textArray: string[];
+    responseArray: string[];
 }
 
 export interface CategoryMeta {
-  data2: {
-    pagination: {
-      page: number;
-      pageCount: number;
-      pageSize: number;
-      total: number;
+    data2: {
+        pagination: {
+            page: number;
+            pageCount: number;
+            pageSize: number;
+            total: number;
+        };
     };
-  };
 }
 
 export interface Categoria {
-  attributes: {
-    category: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    description: string;
-    linkURL: string;
-  };
+    attributes: {
+        category: string;
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+        description: string;
+        linkURL: string;
+    };
 }
 
 export interface ElementProps {
-  id: number;
-  attributes: {
-    category: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    description: string;
-    linkURL: string;
-    fotoCategoria: any;
-  };
+    id: number;
+    attributes: {
+        category: string;
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+        description: string;
+        linkURL: string;
+        fotoCategoria: any;
+    };
 }
 
 export type Element = ElementProps[] | ElementProps;
