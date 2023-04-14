c2fe301063d10ccb0b67fc49ad0aa9be83622ff9
shyvesCCD <pedroochavess97@gmail.com>
2023-04-14 13:02:52 -0300

    feat: function to send emails.

---
+++ pages/api/mailer.ts
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const sgMail = require("@sendgrid/mail");
+
+const { SENDGRID_API_KEY, FROM_EMAIL, TO_EMAIL } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+export default async function handler(req: any, res: any) {
+    console.log(req);
+    const { name, email, message, category } = req.body;
+
+    const msg = {
+        to: TO_EMAIL,
+        from: FROM_EMAIL,
+        subject: `Nova resposta de email para projeto: ${category}`,
+        html: `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong> ${message}</p>
+        `,
+    };
+
+    await sgMail.send(msg);
+
+    res.json({ sucess: true });
+}
