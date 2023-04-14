4f578ad970e16eb4a11688d92b4ec8bafa27fca4
shyvesCCD <pedroochavess97@gmail.com>
2023-04-14 13:33:54 -0300

    feat: add a confirmation email for the client

--- pages/api/mailer.ts
+++ pages/api/mailer.ts
@@ -21,7 +21,21 @@ export default async function handler(req: any, res: any) {
         `,
     };
 
+    const msg2 = {
+        to: email,
+        from: FROM_EMAIL,
+        cc: "pedro_chaves98@hotmail.com",
+        subject: `O que foi respondido no formulário.`,
+        html: `
+        Caso queira validar o que foi escrito no seu formulário:
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong> ${message}</p>
+        `,
+    };
+
     await sgMail.send(msg);
+    await sgMail.send(msg2);
 
     res.json({ sucess: true });
 }
