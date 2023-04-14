fd1f2936cd09418ccf177f469dc54f61b98507a7
shyvesCCD <pedroochavess97@gmail.com>
2023-04-14 20:38:15 -0300

    style: formatting file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Raleway"],
-        body: ['"Raleway"'],
-      },
-      padding: {
-        aspect: "56.25%",
-      },
-      scrollSnapType: {
-        y: "y mandatory",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Raleway"],
+                body: ['"Raleway"'],
+            },
+            padding: {
+                aspect: "56.25%",
+            },
+            scrollSnapType: {
+                y: "y mandatory",
+            },
+        },
     },
-  },
-  variants: {
-    extend: {
-      display: ["group-focus"],
-      opacity: ["group-focus"],
-      inset: ["group-focus"],
+    variants: {
+        extend: {
+            display: ["group-focus"],
+            opacity: ["group-focus"],
+            inset: ["group-focus"],
+        },
     },
-  },
-  plugins: [require("tailwind-hamburgers")],
+    plugins: [require("tailwind-hamburgers")],
 };
