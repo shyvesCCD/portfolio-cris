94e8092c00afb0664517595cf0d1bf4dc2a70a44
shyvesCCD <pedroochavess97@gmail.com>
2023-05-01 19:53:46 -0300

    feat: i18n for multiple locales language

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["https://cris-admin.onrender.com/"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  }
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["https://cris-admin.onrender.com/"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+    i18n: {
+        locales: ["en", "es", "pt-BR", "ko-KR"],
+        defaultLocale: "en",
+        localeDetection: false,
+    },
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
